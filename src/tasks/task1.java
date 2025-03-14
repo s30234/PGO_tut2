82ef2ccf2d75227a8995e24fc599adc4f0e6a4b7
s30234 <s30234@pjwstk.edu.pl>
2025-03-14 14:48:18 +0100

    First task

--- src/tasks/task1.java
+++ src/tasks/task1.java
@@ -1,5 +1,21 @@
 package tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class task1 {
-    
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(filterEvenIndexOddValue(arr)));
+    }
+    public static int [] filterEvenIndexOddValue(int[] array) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] % 2 != 0 && i % 2 == 0) {
+                result.add(array[i]);
+            }
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
 }
